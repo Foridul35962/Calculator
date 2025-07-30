2f9a5924405a695d1c50b677d4171a341105ea27
Foridul35962 <foridulislam35962@gmail.com>
2025-07-31 03:02:37 +0600

    size define

--- src/calculator.jsx
+++ src/calculator.jsx
@@ -5,7 +5,7 @@ import Buttons from "./buttons";
 const calculator = () => {
     return (
         <>
-        <div className="bg-gray-300 text-black rounded-2xl flex justify-center items-center flex-col p-3 dark:bg-gray-800 dark:text-white min-w-sm">
+        <div className="bg-gray-300 text-black rounded-2xl flex justify-center items-center flex-col p-3 dark:bg-gray-800 dark:text-white w-80">
             <Navbars></Navbars>
             <Display></Display>
             <Buttons pera={items}></Buttons>
