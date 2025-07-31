3b6e8f614c25a422979ea9f54459b712e121f1d0
Foridul35962 <foridulislam35962@gmail.com>
2025-07-31 12:37:21 +0600

    optimize code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import Calculator from "./calculator";
+import Navbars from "./navbar";
+import Display from "./display";
+import items from "./buttonsItem";
+import Buttons from "./buttons";
+import { useState } from "react";
 const App = () => {
+  const [input, setInput] = useState("");
   return (
     <div className="h-dvh flex items-center justify-center">
-      <Calculator></Calculator>
+      <div className="bg-gray-300 text-black rounded-2xl flex justify-center items-center flex-col p-3 dark:bg-gray-800 dark:text-white w-80">
+        <Navbars></Navbars>
+        <Display input={input} setInput={setInput}></Display>
+        <Buttons pera={items} input={input} setInput={setInput}></Buttons>
+      </div>
     </div>
   )
 }
